8c810e9a794b0dc4b211f21cdc85ec3b6b937a83
CVCarter <christinevirginiacarter@gmail.com>
2024-09-19 12:46:29 -0700

    finished exercises

--- Ch4Exercises2.py
+++ Ch4Exercises2.py
@@ -22,29 +22,47 @@ def polyline(n, length, angle):
 def equilateral():
     polyline(3,100,120)
 
-def isosceles(leg, bangle):
-    base = 2 * leg * math.sin(math.radians(90 - bangle))
-    angle = 180 - bangle
+def isosceles(leg, base_angle):
+    base = 2 * leg * math.sin(math.radians(90 - base_angle))
+    angle = 180 - base_angle
     polyline(1, leg, angle)
     polyline(1, base, angle)
-    polyline(1, leg, 2 * bangle)
+    polyline(1, leg, 2 * base_angle)
 
-def draw_pie(n, leg2):
+def draw_pie(n, leg):
     base_angle = 90 - 180 / n
     center_angle = 2 * base_angle
     for i in range(n):
-        isosceles(leg2, base_angle)
+        isosceles(leg, base_angle)
         t.left(180 - center_angle)
 
- draw_pie(5, 100)
-
-
-
-
-
+draw_pie(5, 50)
+moveover(150)
+draw_pie(6, 70)
+moveover(150)
+draw_pie(7, 30)
 
 #Exercise bottom of pg52
 
+def arc(radius, angle):
+    arc_length = 2 * math.pi * radius * angle / 360
+    n = 30
+    length = arc_length / n
+    step_angle = angle / n
+    polyline(n, length, step_angle)
+
+def petal():
+    for i in range(2):
+        arc(100, 100)
+        t.left(80)
+
+def draw_flower(n, radius):
+    for i in range(n):
+        petal()
+        t.left(180 - radius)
+
+moveover(-550)
+draw_flower(9, 140)
 
 # Close the turtle graphics window when clicked
 turtle.exitonclick()
